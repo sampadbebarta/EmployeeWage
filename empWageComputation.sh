bd121a0dfe3b101d234109e8a89b927f6b4dc789
Sampad <bebartasampad@gmail.com>
2021-01-06 18:37:51 +0530

    Created Employee Wage file with Welcome message

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
 echo "Welcome to Employee Wage Program"
+
+
+isPresent=1
+checkAttendance=$((RANDOM%2))
+
+if [ $isPresent -eq $checkAttendance ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
