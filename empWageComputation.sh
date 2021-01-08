fb04d4735fa8d5db9dbdfc18ec7332ed9fb78809
Sampad <bebartasampad@gmail.com>
2021-01-08 15:08:43 +0530

    Employee Wage Calculation For A Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,24 +1,28 @@
-#!/bin/bash
-
-echo "Welcome to Employee Wage Program"
-
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-empCheck=$((RANDOM%3));
-case $empCheck in
- 	$isFullTime)
-		echo "Employee is Full Time"
-		empHrs=8;
-			;;
-	$isPartTime)
-		echo "Employee is Part Time"
- 		empHrs=4;
-			;;
-		*)
-		echo "Emplyee is Absent"
-		empHrs=0;
-			;;
-esac
-salary=$(($empHrs*$empRatePerHr));
+#!/bin/bash -x
+isparttime=1;
+isfulltime=2;
+totalsalary=0;
+emprateperHr=20;
+numworkingdays=20;
+for (( day=1; day<=$numworkingdays; day++ ))
+do
+empcheck=$(( RANDOM%3 ))
+        case $empcheck in
+                $isfulltime)
+								echo "Employee is Full Time"
+ 								empHrs=8
+                                ;;
+                $isparttime)
+								echo "Employee is Part Time"
+                        empHrs=4
+                                ;;
+                *)
+								echo "Employee is Absent"
+                        empHrs=0
+                                ;;
+        esac
+        salary=$(( $empHrs*$emprateperHr ));
+        totalsalary=$(( $totalsalary+$salary ));
+done
 echo "Salary is: "$salary
+echo "Total Salary is: "$totalsalary
