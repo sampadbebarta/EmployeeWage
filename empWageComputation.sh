2575cc2532facc32d6409ecc439fec93f90f6b5b
Sampad <bebartasampad@gmail.com>
2021-01-08 15:20:05 +0530

    Employee Total Working Hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,28 +1,34 @@
-#!/bin/bash -x
+#!/bin/bash
+
 isparttime=1;
 isfulltime=2;
-totalsalary=0;
+max_Hrs_Mon=10;
 emprateperHr=20;
 numworkingdays=20;
-for (( day=1; day<=$numworkingdays; day++ ))
+totalEmpHr=0;
+totalworkingdays=0;
+
+while [[ $totalEmpHr -lt $max_Hrs_Mon &&
+	 $totalworkingdays -lt $numworkingdays ]]
 do
-empcheck=$(( RANDOM%3 ))
+  ((totalworkingdays++))
+	empcheck=$(( RANDOM%3 ))
         case $empcheck in
                 $isfulltime)
-								echo "Employee is Full Time"
- 								empHrs=8
+                        echo "Employee is Full Time"
+                        empHrs=8
                                 ;;
                 $isparttime)
-								echo "Employee is Part Time"
+                        echo "Employee is Part Time"
                         empHrs=4
                                 ;;
                 *)
-								echo "Employee is Absent"
+                        echo "Employee is Absent"
                         empHrs=0
                                 ;;
         esac
-        salary=$(( $empHrs*$emprateperHr ));
-        totalsalary=$(( $totalsalary+$salary ));
+	totalEmpHr=$(($totalEmpHr*$empHrs))
 done
-echo "Salary is: "$salary
-echo "Total Salary is: "$totalsalary
+
+totalSalary=$(($totalEmpHr*$emprateperHr))
+echo "Total Salary is: "$totalSalary
